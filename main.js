f6ca643b0827e686eb8767ef9c5f86add74f54fa
ayub432 <ayubmars9@gmail.com>
2024-11-09 18:04:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = 'Ismingizni kiriting';
+console.log = prompt(ism);
+let familya = 'Familyangizni kiriting';
+console.log = prompt(familya);
+let yosh = 'Yoshingizni kiriting';
+console.log = prompt(yosh);
+let fan = 'Yoqtirgan faningizni yozing';
+console.log = prompt(fan, 'Iformatika');
+let joy = 'Yashaydigan tumaningizni kiriting';
+console.log = prompt(joy);
+let user = 'Malumotlaringizni kiritgan uchun tashakur' ;
+alert(user);
